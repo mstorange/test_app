62c5aceb7e3b0dbf5e48b5315d912741b4a7b411
mstorange <ma.stutz@losinger-marazzi.ch>
2026-01-07 16:19:27 +0100

    Update app1.py

--- app1.py
+++ app1.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import streamlit as st
+import folium
 
 st.title('Hier ist eine Testapp, welche Daten von Github nimmt und von allen Usern mit Link genutzt werden kann')
 
@@ -21,5 +22,21 @@ df_filtered = df[(df['bfsnr'] < oben) & (df['bfsnr'] >= unten)]
 
 
 st.table(df_filtered, border=True)
+
+st.subheader('Eine Foliumkarte:')
+
+satellite = folium.TileLayer(
+        tiles = 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
+        attr = 'Esri',
+        name = 'Esri Satellite',
+        overlay = False,
+        control = True)
+
+m = folium.Map(location=[47.04, 8.29], zoom_start=10, tiles=satellite, zoom_control=False)
+folium.Marker([47.12, 8.45], popup='Schweiz').add_to(m)
+
+st_data = st_folium(m, width = 700, height = 500)
+
+
 
 
